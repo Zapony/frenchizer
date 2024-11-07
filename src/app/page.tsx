1bdf386a376d726db18de81af5196e7e4c8570bb
Vaibhav Bhardwaj <vaibhavbhardwaj1230@gmail.com>
2024-11-07 16:46:05 +0200

    refactor: ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import { TranslateView } from "@/components";
 export default function Home() {
   return (
     <div className="h-screen flex items-center justify-center gap-6">
-      <TranslateView />
-      <TranslateView />
+      <TranslateView isFrom={true} language="English" />
+      <TranslateView isFrom={false} language="French" />
     </div>
   );
 }
--- src/components/translate-view/TranslateView.tsx
+++ src/components/translate-view/TranslateView.tsx
@@ -3,18 +3,27 @@
 import React, { useState } from 'react'
 import { BsGlobe2 } from "react-icons/bs";
 
-const TranslateView = () => {
+type TranslateViewProps = {
+    // var to check if it is to or from
+    isFrom: boolean;
+    language: string;
+}
+
+const TranslateView = ({
+    isFrom,
+    language
+}: TranslateViewProps) => {
     const [isDropdownOpen, setDropdownOpen] = useState(false);
-    const [selectedOption, setSelectedOption] = useState('English');
+    const [selectedOption, setSelectedOption] = useState(language);
     const [charCount, setCharCount] = useState(0);
     return (
-        <div className="flex items-center justify-center rounded-full bg-white">
-            <div className="w-full max-w-xl p-6 bg-white rounded-lg shadow-lg">
+        <div className="w-full max-w-xl p-6 bg-white rounded-3xl shadow-lg">
+            <div className='p-5'>
                 <div className="flex items-center gap-4 mb-4">
-                    <span className="text-sm font-semibold text-gray-500">From:</span>
+                    <span className="text-xs font-semibold text-gray-500">{isFrom ? 'From' : 'To'}:</span>
                     <div className="relative w-full">
                         <div
-                            className="flex items-center justify-between w-full py-3 px-5 bg-background rounded-full cursor-pointer"
+                            className="flex items-center justify-between w-full py-3 px-5 bg-background rounded-full"
                         >
                             <BsGlobe2 className="w-5 h-5" />
                             <span className="ml-2 flex-1 px-2 text-black">{selectedOption}</span>
@@ -30,8 +39,13 @@ const TranslateView = () => {
                         className="w-full p-3 bg-transparent border-none outline-none text-lg text-gray-700 font-bold resize-none"
                         rows={10}
                         cols={50}
+                        readOnly={!isFrom}
                     />
-                    <div className="absolute bottom-0 right-0 p-2 text-sm text-gray-500">{charCount} / 5000</div>
+                    {
+                        isFrom && (
+                            <div className="absolute bottom-0 right-0 p-2 text-sm text-gray-500">{charCount} / 5000</div>
+                        )
+                    }
                 </div>
 
                 <div className="flex flex-col items-center pt-4 border-t-2 border-gray-200">
